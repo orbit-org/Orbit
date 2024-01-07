e48a9581dabb65e1f8184c9372bc8d71f8a66bcb
TheInfernalNick <theinfernalnick@gmail.com>
2024-01-07 12:20:44 +0100

    Added promise

--- web/src/routes/user/route.ts
+++ web/src/routes/user/route.ts
@@ -1,9 +1,11 @@
+import { supernova } from "@algorithms/supernova";
 import { NextFunction, Request, Response, Router } from "express";
 
 const app: Router = Router();
 
-app.get("/", (req: Request, res: Response, next: NextFunction) => {
+app.get("/", async (req: Request, res: Response, next: NextFunction) => {
    console.log(res.locals.uid);
+   console.log(await supernova("Test"))
    res.json({});
 });
 
